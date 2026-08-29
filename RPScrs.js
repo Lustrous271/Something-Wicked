e9a09073a70ac23b4bc23aa12bccdeddc9b56f14
Lustrous271 <sina.h363636@gmail.com>
2026-08-29 15:12:00 +0530

    3hourscussyntax

--- RPScrs.js
+++ RPScrs.js
@@ -1,7 +1,37 @@
 function CalcRock() {
-    resulttag = document.getElementById("result")
-    random = Math.floor(Math.random() * 10000 % 3);
-    if(random = 0) {
-     resulttag.innertext = "It's a Tie!"
+    let result_tag = document.getElementById("results")
+    random = Math.floor(Math.random() *3);
+    if(random == 0) {
+     result_tag.innerText = "It's a Tie!"
+    } else if(random == 1) {
+     result_tag.innerText = "You Lose!"   
+    } else if(random == 2) {
+     result_tag.innerText = "You Win!"
+    }
+}
+function CalcPaper() {
+    let result_tag = document.getElementById("results")
+    random = Math.floor(Math.random() * 3);
+    if(random == 0) {
+     result_tag.innerText = "You Win!"
+    }
+    else if(random == 1) {
+     result_tag.innerText = "It's a Tie!"   
+    }
+    else if(random == 2) {
+     result_tag.innerText = "You Lose!"
+    }
+}
+function CalcScissor() {
+    let result_tag = document.getElementById("results")
+    random = Math.floor(Math.random() * 3);
+    if(random == 0) {
+     result_tag.innerText = "You Lose!"
+    }
+    else if(random == 1) {
+     result_tag.innerText = "You Win!"   
+    }
+    else if(random == 2) {
+     result_tag.innerText = "It's a Tie"
     }
 }

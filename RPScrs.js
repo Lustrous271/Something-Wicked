81cc2694cb037f43170b656d0899088b35011309
Lustrous271 <sina.h363636@gmail.com>
2026-08-29 21:29:57 +0530

    500elseif

--- RPScrs.js
+++ RPScrs.js
@@ -1,37 +1,50 @@
+yourscore = 0
 function CalcRock() {
     let result_tag = document.getElementById("results")
+    let ichoose = document.getElementById("ichoose")
     random = Math.floor(Math.random() *3);
     if(random == 0) {
      result_tag.innerText = "It's a Tie!"
+     ichoose.innerText = "I choose Rock."
     } else if(random == 1) {
-     result_tag.innerText = "You Lose!"   
+     result_tag.innerText = "You Lose!"
+     ichoose.innerText = "I choose Paper."   
     } else if(random == 2) {
      result_tag.innerText = "You Win!"
+     ichoose.innerText = "I choose Scissors."
     }
 }
 function CalcPaper() {
     let result_tag = document.getElementById("results")
+    let ichoose = document.getElementById("ichoose")
     random = Math.floor(Math.random() * 3);
     if(random == 0) {
      result_tag.innerText = "You Win!"
+     ichoose.innerText = "I choose Rock."
     }
     else if(random == 1) {
-     result_tag.innerText = "It's a Tie!"   
+     result_tag.innerText = "It's a Tie!" 
+     ichoose.innerText = "I choose Paper."  
     }
     else if(random == 2) {
      result_tag.innerText = "You Lose!"
+     ichoose.innerText = "I choose Scissors."
     }
 }
 function CalcScissor() {
     let result_tag = document.getElementById("results")
+    let ichoose = document.getElementById("ichoose")
     random = Math.floor(Math.random() * 3);
     if(random == 0) {
      result_tag.innerText = "You Lose!"
+     ichoose.innerText = "I choose Rock."
     }
     else if(random == 1) {
      result_tag.innerText = "You Win!"   
+     ichoose.innerText = "I choose Paper."
     }
     else if(random == 2) {
      result_tag.innerText = "It's a Tie"
+     ichoose.innerText = "I choose Scissors."
     }
 }
